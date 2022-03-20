483bc54048efc50164912793bd36d53157ee8040
HKgame01 <74695734+HKgame01@users.noreply.github.com>
2022-03-20 18:02:38 +0530

    Final

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of these crops is typically a perennial rather than an annual?"
+    ,answer: "Apples",
+    options: [
+    "Potatoes",
+    "Apples",
+    "Wheat",
+    "Oats",
+    ,],
+    }
+        ,{
+    numb: 2,
+    question: "Why are farmers primarily so interested in data regarding animal units?",
+    answer: "Grazing",
+    options: [
+    "Calculating sales",
+    "Tilling",
+    "Tractor sizing",
+    "Grazing",
+    ]
+    },
+    {
+    numb: 3,
+    question: "Which of these would a farmer describe as a heifer?",
+    answer: "Young female cow",
+    options: [
+    "Newborn calf of either sex",
+    "Young male cow",
+    "Young female cow",
+    "Really fat old cow",
+    ]
+},
+    {
+    numb: 4,
+    question: "What does a wise farmer do with a supply of dent corn?",
+    answer: "Feed it to livestocks",
+    options: [
+    "Burn it",
+    "Feed it to livestocks",
+    "Throw it away",
+    "Eat it for dinner",
+    ]
+    },
+    {
+    numb: 5,
+    question: "Which of these pH ranges is ideal for most crops?",
+    answer: "6.0 to 7.0",
+    options: [
+    "6.0 to 7.0",
+    "2.0 to 3.0",
+    "9.0 to 10.0",
+    "4.0 to 5.0"
+    ]
+    },
+];
